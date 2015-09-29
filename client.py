976f7c64bc377e2db85a4dc0f5e5c2cd13e71e12
bolan2014 <1139217488@qq.com>
2015-09-29 14:15:27 +0800

    you can input different things and see the response.

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
     sock.connect(('localhost', 8001))  
     import time  
-    time.sleep(2)  
-    sock.send('1')  
+    time.sleep(1)  
+    sock.send(raw_input('please input:'))  
     print sock.recv(1024)  
     sock.close()  
